3d813664e8f87c3cb1564800e2a73e735cd486dd
AlexejheroYTB <alexejhero@gmail.com>
2019-06-27 17:16:20 +0300

    Fiz bug with restrictions

--- src/restrictions.js
+++ src/restrictions.js
@@ -4,7 +4,6 @@ function disableUnapplicable(event) {
 
   for (var block of blocks) {
     if (!block) continue;
-    if (block.disabled) continue;
     if (!document.restrictions[block.type]) continue;
 
     var messages = [];
