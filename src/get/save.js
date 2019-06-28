553a69b5a208fc349f98d69370b852665494b90e
AlexejheroYTB <alexejhero@gmail.com>
2019-06-28 11:41:58 +0300

    Fix bug

--- src/get/save.js
+++ src/get/save.js
@@ -1,13 +1,11 @@
 if (!fs.existsSync(__dirname + "/data/")) fs.mkdirSync(__dirname + "/data/");
 if (!fs.existsSync(__dirname + "/data/" + req.query.guild)) fs.mkdirSync(__dirname + "/data/" + req.query.guild);
 
-fs.writeFileSync(__dirname + "/data/" + req.query.guild + "/blockly.xml", decodeURIComponent(req.query.xml), {
+fs.writeFileSync(__dirname + "/data/" + req.query.guild + "/blockly.xml", req.query.xml, {
   flag: "w"
 });
-fs.writeFileSync(__dirname + "/data/" + req.query.guild + "/bot.txt", decodeURIComponent(req.query.js), {
+fs.writeFileSync(__dirname + "/data/" + req.query.guild + "/bot.txt", req.query.js, {
   flag: "w"
 });
-
-console.log(req.query);
 
 res.redirect("/?guild=" + req.query.guild);
