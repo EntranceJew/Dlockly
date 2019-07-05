567734cb1d1610bae87b08a1e910d65813a93ea6
AlexejheroYTB <alexejhero@gmail.com>
2019-07-05 19:09:47 +0300

    Remove PROJECT_DOMAIN from .env

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ web.all('*', async (req, res) => {
 });
 
 setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+  http.get(`http://dlockly.glitch.me/`);
 }, 280000);
 
 var events = {
